303df038ca06a3e1f38d4de8772e2e7966090f5d
Hendrix1995 <hgud55@naver.com>
2022-01-28 22:31:25 +0900

    ADD: Toggle 완성

--- custom-component/src/component/Toggle.js
+++ custom-component/src/component/Toggle.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
+    const [isToggleOn, setToggleOn] = useState(false);
+
+    const toggleOnHandler = () => {
+        setToggleOn(!isToggleOn);
+    };
+
     return (
         <section>
             <h2>Toggle</h2>
+            <div className="toggle-container">
+                <div className={isToggleOn ? "toggle-bg-on" : "toggle-bg"} onClick={toggleOnHandler}>
+                    <div className={isToggleOn ? "toggle-circle-on" : "toggle-circle"} />
+                </div>
+                <div className="toggle-text">Toggle Switch {isToggleOn ? "ON" : "OFF"}</div>
+            </div>
         </section>
     );
 }
