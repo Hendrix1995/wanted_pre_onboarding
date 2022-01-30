13caaec5a0b39357e631b14329e09a9313fee7ea
Hendrix1995 <hgud55@naver.com>
2022-01-31 00:56:31 +0900

    ADD: AutoComplete 디자인 및 삭제버튼 추가 ADD: AutoComplete dropdown 추가(기능 미구현)

--- custom-component/src/component/AutoComplete.js
+++ custom-component/src/component/AutoComplete.js
@@ -2,19 +2,54 @@ import React, { useState, useEffect } from "react";
 
 function AutoComplete() {
     const [inputValue, setInputValue] = useState("");
-    const inputValueHandler = (e) => {
-        setInputValue(e.target.value);
+    const [isEmpty, setIsEmpty] = useState(false);
+    const [options, setOptions] = useState(["antique", "vintage", "중고A급", "rustic", "refurbished"]);
+
+    const inputValueHandler = (text) => {
+        if (text) {
+            setIsEmpty(true);
+        } else {
+            setIsEmpty(false);
+        }
+        setInputValue(text);
+
+        const Regex = new RegExp(text, "i");
+        const selectedOption = options.filter((el) => el.match(Regex));
     };
-    const inputClear = () => {
+
+    const dropDownClickHandler = (option) => {
+        setInputValue(option);
+        const selectedOption = options.filter((el) => el === option);
+        setOptions(selectedOption);
+    };
+
+    const deleteInputHandler = () => {
         setInputValue("");
     };
+
+    useEffect(() => {
+        if (inputValue === "") {
+            setIsEmpty(false);
+        }
+    }, [inputValue]);
 
     return (
         <section>
             <h2>Autocomplete</h2>
             <div className="autocomplete-container">
-                <input type="text" className="autocomplete-input" autoComplete="on" onChange={inputValueHandler} />
-                <i className="fas fa-times" onClick={inputClear} />
+                <div className="autocomplete-input-container">
+                    <input type="text" className="autocomplete-input" autoComplete="on" onChange={(e) => inputValueHandler(e.target.value)} value={inputValue} />
+                    <i className="fas fa-times" onClick={deleteInputHandler} />
+                </div>
+                {isEmpty ? (
+                    <div className="dropDownList-box">
+                        {options.map((el, index) => (
+                            <li className="dropDownList" key={index} onClick={dropDownClickHandler}>
+                                {el}
+                            </li>
+                        ))}
+                    </div>
+                ) : null}
             </div>
         </section>
     );
