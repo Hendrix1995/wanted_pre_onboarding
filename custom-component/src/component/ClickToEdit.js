74b9b2edf51d4c7171206226c87a5d3b1f29461f
Hendrix1995 <hgud55@naver.com>
2022-02-02 01:24:04 +0900

    ADD: ClickToEdit 구현 완료

--- custom-component/src/component/ClickToEdit.js
+++ custom-component/src/component/ClickToEdit.js
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
 
 function ClickToEdit() {
+    const [clickToEditValue, setClickToEditValue] = useState({
+        name: "김코딩",
+        age: 20,
+    });
+
+    const onblurHandler = (key) => (e) => {
+        setClickToEditValue({ ...clickToEditValue, [key]: e.target.value });
+    };
+
     return (
         <section>
             <h2>ClickToEdit</h2>
             <div className="click-to-adit-container">
                 <div className="click-to-adit-box">
                     <span>이름</span>
-                    <input type="text" className="click-to-adit-box-input" />
+                    <input type="text" className="click-to-adit-box-input" defaultValue={"김코딩"} onBlur={onblurHandler("name")} />
                 </div>
                 <div className="click-to-adit-box">
                     <span>나이</span>
-                    <input type="text" className="click-to-adit-box-input" />
+                    <input type="number" className="click-to-adit-box-input" defaultValue={20} onBlur={onblurHandler("age")} />
                 </div>
-                <div className="description">이름 최해커 나이 20</div>
+                <div className="description">
+                    이름 {clickToEditValue.name} 나이 {clickToEditValue.age}
+                </div>
             </div>
         </section>
     );
